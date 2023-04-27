c587c39f60e8271a58f05e4ed6bff4615f15a08d
Soleo <devsoleo@protonmail.com>
2023-04-27 11:37:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 print("ok
+
+target = -5
+num = 3
+
+target =- num  # Noncompliant; target = -3. Is that really what's meant?
+target =+ num # Noncompliant; target = 3
+      
+jwt.decode(token, verify = False)  # Noncompliant
+jwt.decode(token, key, options={"verify_signature": False})  # Noncompliant
